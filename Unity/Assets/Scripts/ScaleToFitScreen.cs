49fbdea28fbfb2e297039f1136a8a2c2269ff55d
Slav Pilus <slav.pilus@kps.com>
2022-08-27 20:36:24 +0100

    slot graphics corrected

--- Unity/Assets/Scripts/ScaleToFitScreen.cs
+++ Unity/Assets/Scripts/ScaleToFitScreen.cs
@@ -11,17 +11,19 @@ public class ScaleToFitScreen : MonoBehaviour
         sr = GetComponent<SpriteRenderer>();
 
         // world height is always camera's orthographicSize * 2
-        float worldScreenHeight = Camera.main.orthographicSize * 2;
+        float worldScreenHeight = GameObject.Find("UI Camera").GetComponent<Camera>().orthographicSize * 2;
+        Debug.Log("worldScreenHeight = " + worldScreenHeight);
 
         // world width is calculated by diving world height with screen heigh
         // then multiplying it with screen width
         float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+        Debug.Log("worldScreenWidth = " + worldScreenWidth);
 
         // to scale the game object we divide the world screen width with the
         // size x of the sprite, and we divide the world screen height with the
         // size y of the sprite
         transform.localScale = new Vector3(
             worldScreenWidth / sr.sprite.bounds.size.x,
-            worldScreenHeight / sr.sprite.bounds.size.y, 1);
+            worldScreenHeight / sr.sprite.bounds.size.y, sr.sprite.bounds.size.z);
     }
 }
