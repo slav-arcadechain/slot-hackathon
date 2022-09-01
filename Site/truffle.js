e3fc9d8a9551ebcf392d3dbfca013a9f5b2a81ce
Slav Pilus <slav.pilus@kps.com>
2022-09-01 07:08:34 +0100

    changing contract output dir

--- Site/truffle.js
+++ Site/truffle.js
@@ -4,7 +4,7 @@ const privateKey = fs.readFileSync(".secret").toString().trim();
 const path = require("path");
 
 module.exports = {
-  contracts_build_directory: path.join(__dirname, "app/src/contracts"),
+  contracts_build_directory: path.join(__dirname, "build/contracts"),
   networks: {
     development: {
       host: "127.0.0.1",     // Localhost (default: none)
